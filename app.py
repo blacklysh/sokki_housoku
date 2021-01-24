67a5339125de8a631687669423a59506758dfca2
blacklysh <60771454+blacklysh@users.noreply.github.com>
2021-01-24 14:59:07 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get():
+	return render_template('index.html', \
+		title = '速記法則', \
+		message = '名前を入力して下さい。')
+
+@app.route("/", methods=['POST'])
+def index():
+    housoku = request.form['housoku']
+    return render_template('index.html', housoku=housoku)
