92a74778c7004fcd76daa07b51b4e23d63a44a2a
blacklysh <60771454+blacklysh@users.noreply.github.com>
2021-02-19 22:55:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get():
-	return render_template('index.html', \
+	return render_template('index2.html', \
 		title = '速記法則', \
 		message = '名前を入力して下さい。')
 
 @app.route("/", methods=['POST'])
 def index():
     housoku = request.form['housoku']
-    return render_template('index.html', housoku=housoku)
+    return render_template('index2.html', housoku=housoku)
