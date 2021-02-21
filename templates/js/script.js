febbed43517aefc753beb22e306f37bc9bc007d4
blacklysh <60771454+blacklysh@users.noreply.github.com>
2021-02-21 22:26:53 +0900

    Update script.js

--- templates/js/script.js
+++ templates/js/script.js
@@ -37,5 +37,15 @@ $(function(){
 		$(this).next().slideToggle();
 		$('#menuBtn').removeClass('close');
 	});
+	function(){
+		$('a[href^=#]').click(function() {
+			var speed = 400;
+			var href= $(this).attr("href");
+			var target = $(href == "#" || href == "" ? 'html' : href);
+			var position = target.offset().top;
+			$('body,html').animate({scrollTop:position}, speed, 'swing');
+			return false;
+	});
+	
 
 });
